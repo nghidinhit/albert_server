8d122031513fc13876d4f8d6eaf737daf105b7ce
Nguyen Dinh Nghi <nghidinhit@gmail.com>
2020-06-11 09:17:42 +0700

    refactor

--- convert_tf_to_pytorch.sh
+++ convert_tf_to_pytorch.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+python convert_albert_tf_checkpoint_to_pytorch_v2.py \
+    --tf_checkpoint_path=checkpoints/lm-checkpoint/ngoan_pretrained \
+    --albert_config_file=configs/albert_config_large.json \
+    --pytorch_dump_path=checkpoints/lm-checkpoint/ngoan_pretrained/pytorch_model.bin \
+    --share_type=all
