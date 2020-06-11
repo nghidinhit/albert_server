60a702bd889675bb0954aa0bc9d46963353c2fa3
Nguyen Dinh Nghi <nghidinhit@gmail.com>
2020-06-11 09:19:45 +0700

    first commit

---
+++ albert_interface.py
@@ -0,0 +1,228 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.relpath(__file__)), 'core'))
+import torch
+import random
+import logging
+from model.configuration_bert import BertConfig
+from model.tokenization_bert import BertTokenizer
+from model.modeling_albert import BertForPreTraining, BertModel, BertForSequenceClassification, BertForTokenClassification
+
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class InputExample(object):
+    """A single training/test example for the language model."""
+
+    def __init__(self, guid, tokens_a, tokens_b=None, is_next=None, lm_labels=None):
+        """Constructs a InputExample.
+
+        Args:
+            guid: Unique id for the example.
+            tokens_a: string. The untokenized text of the first sequence. For single
+            sequence tasks, only this sequence must be specified.
+            tokens_b: (Optional) string. The untokenized text of the second sequence.
+            Only must be specified for sequence pair tasks.
+            label: (Optional) string. The label of the example. This should be
+            specified for train and dev examples, but not for test examples.
+        """
+        self.guid = guid
+        self.tokens_a = tokens_a
+        self.tokens_b = tokens_b
+
+
+class InputFeatures(object):
+    """A single set of features of data."""
+
+    def __init__(self, input_ids, input_mask, segment_ids):
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+
+
+def _truncate_seq_pair(tokens_a, tokens_b, max_length):
+    """Truncates a sequence pair in place to the maximum length."""
+
+    # This is a simple heuristic which will always truncate the longer sequence
+    # one token at a time. This makes more sense than truncating an equal percent
+    # of tokens from each, since if one sequence is very short then each token
+    # that's truncated likely contains more information than a longer sequence.
+    while True:
+        total_length = len(tokens_a) + len(tokens_b)
+        if total_length <= max_length:
+            break
+        if len(tokens_a) > len(tokens_b):
+            tokens_a.pop()
+        else:
+            tokens_b.pop()
+
+
+def convert_example_to_features(example, max_seq_length, tokenizer):
+    """
+    Convert a raw sample (pair of sentences as tokenized strings) into a proper training sample with
+    IDs, LM labels, input_mask, CLS and SEP tokens etc.
+    :param example: InputExample, containing sentence input as strings and is_next label
+    :param max_seq_length: int, maximum length of sequence.
+    :param tokenizer: Tokenizer
+    :return: InputFeatures, containing all inputs and labels of one sample as IDs (as used for model training)
+    """
+    tokens_a = example.tokens_a
+    tokens_b = example.tokens_b
+    # Modifies `tokens_a` and `tokens_b` in place so that the total
+    # length is less than the specified length.
+    # Account for [CLS], [SEP], [SEP] with "- 3"
+    _truncate_seq_pair(tokens_a, tokens_b, max_seq_length - 3)
+
+    # The convention in BERT is:
+    # (a) For sequence pairs:
+    #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
+    #  type_ids: 0   0  0    0    0     0       0 0    1  1  1  1   1 1
+    # (b) For single sequences:
+    #  tokens:   [CLS] the dog is hairy . [SEP]
+    #  type_ids: 0   0   0   0  0     0 0
+    #
+    # Where "type_ids" are used to indicate whether this is the first
+    # sequence or the second sequence. The embedding vectors for `type=0` and
+    # `type=1` were learned during pre-training and are added to the wordpiece
+    # embedding vector (and position vector). This is not *strictly* necessary
+    # since the [SEP] token unambigiously separates the sequences, but it makes
+    # it easier for the model to learn the concept of sequences.
+    #
+    # For classification tasks, the first vector (corresponding to [CLS]) is
+    # used as as the "sentence vector". Note that this only makes sense because
+    # the entire model is fine-tuned.
+    tokens = []
+    segment_ids = []
+    tokens.append("[CLS]")
+    segment_ids.append(0)
+    for token in tokens_a:
+        tokens.append(token)
+        segment_ids.append(0)
+    tokens.append("[SEP]")
+    segment_ids.append(0)
+
+    # assert len(tokens_b) > 0
+    for token in tokens_b:
+        tokens.append(token)
+        segment_ids.append(1)
+    tokens.append("[SEP]")
+    segment_ids.append(1)
+
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    # The mask has 1 for real tokens and 0 for padding tokens. Only real
+    # tokens are attended to.
+    input_mask = [1] * len(input_ids)
+
+    # Zero-pad up to the sequence length.
+    while len(input_ids) < max_seq_length:
+        input_ids.append(0)
+        input_mask.append(0)
+        segment_ids.append(0)
+
+    assert len(input_ids) == max_seq_length
+    assert len(input_mask) == max_seq_length
+    assert len(segment_ids) == max_seq_length
+
+    # if example.guid < 5:
+    #     logger.info("*** Example ***")
+    #     logger.info("guid: %s" % (example.guid))
+    #     logger.info("tokens: %s" % " ".join(
+    #             [str(x) for x in tokens]))
+    #     logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+    #     logger.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
+    #     logger.info(
+    #             "segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
+    #     logger.info("LM label: %s " % (lm_label_ids))
+    #     logger.info("Is next sentence label: %s " % (example.is_next))
+
+    features = InputFeatures(input_ids=input_ids,
+                             input_mask=input_mask,
+                             segment_ids=segment_ids)
+    return features
+
+
+def random_word(tokens, tokenizer):
+    """
+    Masking some random tokens for Language Model task with probabilities as in the original BERT paper.
+    :param tokens: list of str, tokenized sentence.
+    :param tokenizer: Tokenizer, object used for tokenization (we need it's vocab here)
+    :return: (list of str, list of int), masked tokens and related labels for LM prediction
+    """
+    output_label = []
+
+    for i, token in enumerate(tokens):
+        prob = random.random()
+        # mask token with 15% probability
+        if prob < 0.15:
+            prob /= 0.15
+
+            # 80% randomly change token to mask token
+            if prob < 0.8:
+                tokens[i] = "[MASK]"
+
+            # 10% randomly change token to random token
+            elif prob < 0.9:
+                tokens[i] = random.choice(list(tokenizer.vocab.items()))[0]
+
+            # -> rest 10% randomly keep current token
+
+            # append current token to output (we will predict these later)
+            try:
+                output_label.append(tokenizer.vocab[token])
+            except KeyError:
+                # For unknown words (should not occur with BPE vocab)
+                output_label.append(tokenizer.vocab["[UNK]"])
+                logger.warning("Cannot find token '{}' in vocab. Using [UNK] insetad".format(token))
+        else:
+            # no masking token (will be ignored by loss function later)
+            output_label.append(-1)
+
+    return tokens, output_label
+
+
+def prepare_bert_input(guid, tokenizer, seq_len, sentence_a, sentence_b=None, is_next_label=None):
+    # tokenize
+    tokens_a = tokenizer.tokenize(sentence_a)
+    tokens_b = tokenizer.tokenize(sentence_b)
+    # combine to one sample
+    cur_example = InputExample(guid=guid, tokens_a=tokens_a, tokens_b=tokens_b, is_next=is_next_label, lm_labels=None)
+    
+    # transform sample to features
+    cur_features = convert_example_to_features(cur_example, seq_len, tokenizer)
+
+    cur_tensors = (torch.tensor(cur_features.input_ids),
+                    torch.tensor(cur_features.input_mask),
+                    torch.tensor(cur_features.segment_ids))
+    
+    return cur_tensors
+
+
+def load_bert(bert_model_path):
+    # tokenizer = BertTokenizer(vocab_file='pretrained/bert_base_news_128/vocab.txt')
+    tokenizer = BertTokenizer.from_pretrained(bert_model_path, do_lower_case=True)
+    # Prepare model
+    # config = BertConfig.from_json_file(args.bert_config_file)
+    # model = BertForPreTraining.from_pretrained(bert_model_path)
+    model = BertModel.from_pretrained(bert_model_path)
+    # model = BertForPreTraining(config)
+    return model, tokenizer
+
+
+if __name__ == '__main__':
+    model_path = 'checkpoints/lm-checkpoint/albert_large_shareall/20191126-step: 851693 - loss: 1.519739'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    bert_model, bert_tokenizer = load_bert(model_path)
+    bert_model.to(device)
+
+    guid = '1'
+    sentence_a = "thủ đô hà nội"
+    input_ids, input_mask, segment_ids = prepare_bert_input(guid, bert_tokenizer, 128, sentence_a)
+    print('input_ids: ', input_ids)
+    outputs = bert_model(input_ids=input_ids.unsqueeze(dim=0).to(device), token_type_ids=segment_ids.to(device))
+    sequence_output, pooled_output = outputs
+    print('pooled_output: ', pooled_output)
